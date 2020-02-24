62cc26d7621330b14892b85b60ad608eaf208e07
Atsushi Watanabe <atsushi.w@ieee.org>
2020-02-24 19:34:59 +0900

    Test without setErrno

--- test.go
+++ test.go
@@ -14,7 +14,7 @@ func main() {
 	for i := 0; i < runtime.NumCPU()*2; i++ { // make sure it run goroutine on all CPUs
 		go func() {
 			time.Sleep(500 * time.Millisecond)
-			C.setErrno()
+			// C.setErrno()
 		}()
 	}
 	if _, err := C.usleep(C.uint(1000000)); err != nil {
