f329359af1efdfd04b0b233510b71dc24e208bfc
Atsushi Watanabe <atsushi.w@ieee.org>
2020-02-24 20:03:54 +0900

    Fix wait

--- test.go
+++ test.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"runtime"
-	"time"
 )
 
 // #include <unistd.h>
 // #include <errno.h>
 // #include <time.h>
-// void setErrno() { errno = 1; }
+// void setErrno() { while (1) { errno = 1; } }
 // void wait() {
-//   time_t n, s = time(0);
+//   time_t n = time(0), s = time(0);
 //   do {
 //     time(&n);
-//   } while(difftime(n, s) < 1.0);
+//   } while(difftime(n, s) < 5.0);
 // }
 import "C"
 
 func main() {
-	for i := 0; i < runtime.NumCPU()*2; i++ { // make sure it run goroutine on all CPUs
+	for i := 0; i < runtime.NumCPU()*2; i++ { // make sure to run on all CPUs
 		go func() {
-			time.Sleep(500 * time.Millisecond)
 			C.setErrno()
 		}()
 	}
+	// sleep/usleep on always sets errno=60 on Darwin
 	if _, err := C.wait(); err != nil {
 		panic(err)
 	}
