741ec9fc188d4ccb51addbfd367bffc9933fd796
Atsushi Watanabe <atsushi.w@ieee.org>
2020-02-24 19:27:38 +0900

    Set errno by cgo

--- test.go
+++ test.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"net"
 	"runtime"
 	"time"
 )
 
 // #include <unistd.h>
+// #include <errno.h>
+// void setErrno() { errno = 1; }
 import "C"
 
 func main() {
 	for i := 0; i < runtime.NumCPU()*2; i++ { // make sure it run goroutine on all CPUs
 		go func() {
 			time.Sleep(500 * time.Millisecond)
-			_, _ = net.Dial("tcp", "localhost:12345") // dst port is not listened
+			C.setErrno()
 		}()
 	}
 	if _, err := C.usleep(C.uint(1000000)); err != nil {
