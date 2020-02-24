fdf7ef206bc4ced765f556ea782bb137d76aabe0
Atsushi Watanabe <atsushi.w@ieee.org>
2020-02-24 18:57:51 +0900

    Minimize the code

--- test.go
+++ test.go
@@ -9,21 +9,19 @@ import (
 )
 
 // #include <unistd.h>
-// #include <errno.h>
-// void wait() { usleep(2000000); }
 import "C"
 
 func main() {
 	for i := 0; i < runtime.NumCPU()*2; i++ { // run goroutine on all CPUs
 		go func() {
 			for {
-				_, _ = net.Dial("tcp", "localhost:12345") // set errno
+				_, _ = net.Dial("tcp", "localhost:12345") // dst port is not listened
 				time.Sleep(time.Millisecond)
 			}
 		}()
 	}
-	_, err := C.wait()
-	fmt.Printf("CGO: %v\n", err)
+	_, err := C.usleep(C.uint(1000000))
+	fmt.Printf("error from CGO: %v\n", err)
 	if err != nil {
 		os.Exit(1)
 	}
